a5e0476c0248017661ee28063d66b87a5b270dba
Brett New and Molly McGlone <brett+molly@>
2016-09-13 11:40:36 -0700

    create session when user logs in and authorize method to force users to log in to take certain actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ private
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def authorize
+    redirect_to new_session_path, alert: "Not authorized" if current_user.nil?
+  end
 end
